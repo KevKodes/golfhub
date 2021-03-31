79b224ef95afce2bd057ca8fc69de7149c4a9a2b
Kevin <pitzer.kevinj@gmail.com>
2021-03-31 17:04:45 -0400

    fix user auth

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { logout } from "../../store/auth";
 
-const LogoutButton = ({setAuthenticated}) => {
+const LogoutButton = ({ setAuthenticated }) => {
+  const dispatch = useDispatch();
   const onLogout = async (e) => {
-    await logout();
+    dispatch(logout());
     setAuthenticated(false);
   };
 
