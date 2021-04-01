edeacd0b2618f63b3866a293bf7e6b412eb6067b
Kevin <pitzer.kevinj@gmail.com>
2021-04-01 17:13:38 -0400

    render on the course page

--- react-app/src/components/CoursePage/index.js
+++ react-app/src/components/CoursePage/index.js
@@ -4,17 +4,45 @@ import { useParams } from 'react-router-dom';
 import './CoursePage.css';
 
 const CoursePage = () => {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
   const {courseId} = useParams()
+  const [pageCourse, setPageCourse] = useState({})
   const courseList = useSelector(state => state.courses?.courseList)
 
-  const pageCourse = courseList?.find(course => course.id == courseId)
+  useEffect(() => {
+    if (courseList) {
+      const foundCourse = courseList.find(course => parseInt(course.id) === parseInt(courseId))
+      setPageCourse(foundCourse)
+    }
+
+  }, [courseList, courseId])
 
   return (
     <div className="course-wrapper">
-      <div>
-        {pageCourse?.courseName}
-      </div>
+      {pageCourse ? (
+        <div className="course-info-wrapper">
+          <div className="course-top">
+            <div className="course-main">
+              <img src={pageCourse.imageURL} alt="course picture" />
+              {pageCourse.courseName}
+              <div className="course-main-description">
+                {pageCourse.description}
+              </div>
+            </div>
+            <div className="course-sidebar">
+              <div className="course-sidebar-header">
+                {pageCourse.courseName}
+                {pageCourse.address}
+              </div>
+            </div>
+          </div>
+          <div className="course-bottom">
+            Scorecard
+          </div>
+        </div>
+      ) : (
+        <h1>Course not found</h1>
+      )}
     </div>
   )
 }
