1929ae502fef42c4c75efa8ae9310765756e1167
Kevin <pitzer.kevinj@gmail.com>
2021-04-07 15:27:38 -0400

    render scorecard top row

--- react-app/src/components/AddScore/ScorecardTop.js
+++ react-app/src/components/AddScore/ScorecardTop.js
@@ -4,7 +4,39 @@ const ScorecardTop = ({ teeData }) => {
 
   return (
     <div className="scorecard-top-wrapper">
-      top part renders
+      <table className="score-card">
+        <thead>
+          <tr>
+            <th><div className="hole-col">HOLE</div></th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+            <th>8</th>
+            <th>9</th>
+            <th>OUT</th>
+            <th>10</th>
+            <th>11</th>
+            <th>12</th>
+            <th>13</th>
+            <th>14</th>
+            <th>15</th>
+            <th>16</th>
+            <th>17</th>
+            <th>18</th>
+            <th>IN</th>
+            <th>TOT</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th className="wide-col">INDEX</th>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
